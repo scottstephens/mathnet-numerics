d7062f04ab48da7f2ebe68d10a3f253058d47fdf
Scott Stephens <stephens.js@gmail.com>
2014-01-04 16:34:42 -0600

    Optimization: update tests to use new BfgsMinimizer constructor

--- src/UnitTests/OptimizationTests/TestBfgsMinimizer.cs
+++ src/UnitTests/OptimizationTests/TestBfgsMinimizer.cs
@@ -17,7 +17,7 @@ namespace MathNet.Numerics.UnitTests.OptimizationTests
         public void FindMinimum_Rosenbrock_Easy()
         {
             var obj = new SimpleObjectiveFunction(RosenbrockFunction.Value, RosenbrockFunction.Gradient);
-            var solver = new BfgsMinimizer(1e-5, 1000);
+            var solver = new BfgsMinimizer(1e-5, 1e-5, 1000);
             var result = solver.FindMinimum(obj, new MathNet.Numerics.LinearAlgebra.Double.DenseVector(new double[] { 1.2, 1.2 }));
 
             Assert.That(Math.Abs(result.MinimizingPoint[0] - 1.0), Is.LessThan(1e-3));
@@ -28,7 +28,7 @@ namespace MathNet.Numerics.UnitTests.OptimizationTests
         public void FindMinimum_Rosenbrock_Hard()
         {
             var obj = new SimpleObjectiveFunction(RosenbrockFunction.Value, RosenbrockFunction.Gradient);
-            var solver = new BfgsMinimizer(1e-5, 1000);
+            var solver = new BfgsMinimizer(1e-5, 1e-5, 1000);
             var result = solver.FindMinimum(obj, new MathNet.Numerics.LinearAlgebra.Double.DenseVector(new double[] { -1.2, 1.0 }));
 
             Assert.That(Math.Abs(result.MinimizingPoint[0] - 1.0), Is.LessThan(1e-3));
@@ -39,7 +39,7 @@ namespace MathNet.Numerics.UnitTests.OptimizationTests
         public void FindMinimum_Rosenbrock_Overton()
         {
             var obj = new SimpleObjectiveFunction(RosenbrockFunction.Value, RosenbrockFunction.Gradient);
-            var solver = new BfgsMinimizer(1e-5, 1000);
+            var solver = new BfgsMinimizer(1e-5, 1e-5, 1000);
             var result = solver.FindMinimum(obj, new MathNet.Numerics.LinearAlgebra.Double.DenseVector(new double[] { -0.9, -0.5 }));
 
             Assert.That(Math.Abs(result.MinimizingPoint[0] - 1.0), Is.LessThan(1e-3));
